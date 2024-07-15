6e90a38585bd21e88b71630f0f12dfc5658371b7
Monica <fanmn72@gmail.com>
2024-07-15 07:46:45 -0700

    working on string reconstruct

--- stringReconstitute/stringConstruct.py
+++ stringReconstitute/stringConstruct.py
@@ -1,5 +1,44 @@
+from collections import defaultdict
+
+class Graph:
+    def __init__(self, num_vertices):
+        self.vertices = num_vertices
+        self.graph = defaultdict(list)
+        self.IN = [0] * self.vertices
+        self.verticesList = []
+    def addEdge(self, start, end):
+        self.graph[start].append(end)
+        self.verticesList.append(start)
+        self.verticesList.append(end)
+
 def deBruijn(Patterns, k):
+    deBruijnGraph = Graph(len(Patterns) * 2)
+    for pattern in Patterns:
+        prefix = pattern[: (k - 1)]
+        suffix = pattern[1:]
+        deBruijnGraph.addEdge(prefix, suffix)
+    return deBruijnGraph
+
+def DFSUtil(start_vertex, graph, visited):
+    visited[start_vertex] = True
+    for neighbor in graph[start_vertex]:
+        if visited[neighbor] == False:
+            DFSUtil(neighbor, graph, visited)
+    return 0
+
+def EulerianPath(dBGraph, verticesList):
+    # graph is strongly connected components
+    visited = {key: False for key in verticesList}
+    start_vertex = -1
+    for vertex in dBGraph:
+        if dBGraph[vertex]:
+            start_vertex = vertex
+            break
     
+    # depth first search
+    DFSUtil(start_vertex, dBGraph, visited)
+    
+    # each vertex has same INdegree and OUTdegree (balanced)
     return 0
 
 def stringConstruct(Patterns):
@@ -15,6 +54,11 @@ if __name__ == "__main__":
                 "GGCT",
                 "GCTT",
                 "TTAC"]
-    print(patterns)
+
+    graph = {'CTT': ['TTA'], 'ACC': ['CCA'], 'TAC': ['ACC'], 'GGC': ['GCT'], 'GCT': ['CTT'], 'TTA': ['TAC']}
+    testGraph = deBruijn(patterns, k)
+    print(testGraph.graph)
+    vertices_list = testGraph.verticesList
+    EulerianPath(graph, vertices_list)
 
 # GGCTTACCA
