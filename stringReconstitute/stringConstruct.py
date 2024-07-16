535e8f37b21642937c856d97bbcc66dc70a60ffd
Monica <fanmn72@gmail.com>
2024-07-15 20:42:41 -0700

    working on string construct

--- stringReconstitute/stringConstruct.py
+++ stringReconstitute/stringConstruct.py
@@ -1,5 +1,11 @@
 from collections import defaultdict
 
+def DFS(start_vertex, adjacent_list, visited):
+    visited[start_vertex] = True
+    for neighbor in adjacent_list[start_vertex]:
+        if visited[neighbor] == False:
+            DFS(neighbor, adjacent_list, visited)
+
 class Node:
     def __init__(self, content):
         self.content = content
@@ -34,13 +40,21 @@ class Graph:
 
     # need to delete print function
     def isBalanced(self):
+        num_start_node = 0
+        num_end_node = 0
         for node in self.nodes.values():
-            if node.INdegree != node.OUTdegree:
+            if node.INdegree - node.OUTdegree == 1:
+                num_end_node += 1
+            elif node.OUTdegree - node.INdegree == 1:
+                num_start_node += 1
+            elif node.INdegree != node.OUTdegree:
                 ####
                 print(node.content)
+                print(node.INdegree)
+                print(node.OUTdegree)
                 ###
                 return False
-        return True
+        return (num_start_node == 1 and num_end_node == 1) or (num_start_node == 0 and num_end_node == 0)
 
     def is_strongly_connected(self):
         # perform depth first search
@@ -48,26 +62,52 @@ class Graph:
         iterator = iter(self.nodes)
         start_node = next(iterator)
         visited = {node: False for node in self.nodes}
-        
-        def DFS(vertex):
-            visited[vertex] = True
-            for neighbor in self.adjacent_list[vertex]:
-                if not visited[neighbor]:
-                    DFS(neighbor)
-        DFS(start_node)
 
-        if not all(visited.values()) == True:
+        DFS(start_node, self.adjacent_list, visited)
+        print("SCC: " + str(all(visited.values())))
+        if not all(visited.values()):
+            
             return False
         
         # check if all nodes can reach the starting node (transpose graph)
-        transpose_adj_list = 
+        transpose_adjacent_list = {node: [] for node in self.nodes}
+        for node, neighbors in self.adjacent_list.items():
+            for neighbor in neighbors:
+                transpose_adjacent_list[neighbor].append(node)
+        
+        visited = {node: False for node in self.nodes}
+        DFS(start_node, transpose_adjacent_list, visited)
+        print("SCC: " + str(all(visited.values())))
+        return all(visited.values())
+
+    def find_eulerian_path(self):
+        if not self.isBalanced() or not self.is_strongly_connected():
+            print("no eulerian path")
+            return None
+        
+        start_node = next((node.content for node in self.nodes.values() if node.OUTdegree - node.INdegree == 1), None)
+        if not start_node:
+            start_node = next(iter(self.nodes))        
+        
+        path = []
+        stack = [start_node]
+
+        while stack:
+            u = stack[-1]
+            if self.adjacent_list[u]:
+                v = self.adjacent_list[u].pop()
+                stack.append(v)
+            else:
+                path.append(stack.pop())
+        path.reverse()
+        return path
 
     def print_graph(self):
         for edge in self.edges:
             print(f"{edge.start.content} -> {edge.end.content}")
     
-
-def deBruijn(patterns):
+# delete print function
+def deBruijn(patterns, k):
     dBGraph = Graph()
     for pattern in patterns:
         prefix = pattern[: (k - 1)]
@@ -75,23 +115,43 @@ def deBruijn(patterns):
         dBGraph.add_node(prefix)
         dBGraph.add_node(suffix)
         dBGraph.add_edge(prefix, suffix)
+    ####
     dBGraph.print_graph()
+    ###
     return dBGraph
 
 
 
 if __name__ == "__main__":
-    k = 4
+    # k = 4
 
-    patterns = ["CTTA",
-                "ACCA",
-                "TACC",
-                "GGCT",
-                "GCTT",
-                "TTAC"]
+    # patterns = ["CTTA",
+    #             "ACCA",
+    #             "TACC",
+    #             "GGCT",
+    #             "GCTT",
+    #             "TTAC",
+    #             "TACG"]
     
-    dB = deBruijn(patterns)
+    # dB = deBruijn(patterns)
+    # print(dB.isBalanced())
+    # print("if strongly connected: ")
+    # print(dB.is_strongly_connected())
+    # print(dB.find_eulerian_path())
+
+    # patterns = []
+    # with open("patterns.txt", "r") as file:
+    #     lines = file.readlines()
+    #     for line in lines:
+    #         patterns.append(line.strip())
+    # k = 15
+    # dB = deBruijn(patterns)
+    # print(dB.isBalanced())
+
+    patterns = ["AAC", "ACG", "GAA", "GTT", "TCG"]
+    k = 3
+    dB = deBruijn(patterns, k)
     print(dB.isBalanced())
-    print(dB.is_strongly_connected())
+
 
 # GGCTTACCA
