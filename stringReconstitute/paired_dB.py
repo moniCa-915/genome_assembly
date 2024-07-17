7b399f8c5429b561a8d39ac66a2213060df42f75
Monica <fanmn72@gmail.com>
2024-07-17 15:43:03 -0700

    working

---
+++ stringReconstitute/paired_dB.py
@@ -0,0 +1,14 @@
+def paired_dB(text, k , d):
+    paired_dB = []
+    for i in range(len(text)):
+        first_k_mer = text[i: i + k]
+        last_k_mer = text[]
+    
+    return 0
+
+if __name__ == "__main__":
+    text = "TAATGCCATGGGATGTT"
+    k = 3
+    d = 2
+    paired_dB(text, 3, 2)
+
